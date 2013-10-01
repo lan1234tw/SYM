1a6e27c10474314db8e7bbd2d9d5db29ca8eac5d
lan1234tw <hsiuyi@HsiuYitekiMacBook-Pro.local>
2013-10-01 23:04:57 +0800

    Add navigation controller

---
+++ SYM/messageViewController.h
@@ -0,0 +1,13 @@
+//
+//  messageViewController.h
+//  SYM
+//
+//  Created by HsiuYi on 13/10/1.
+//  Copyright (c) 2013年 HsiuYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface messageViewController : UIViewController
+
+@end
---
+++ SYM/messageViewController.m
@@ -0,0 +1,40 @@
+//
+//  messageViewController.m
+//  SYM
+//
+//  Created by HsiuYi on 13/10/1.
+//  Copyright (c) 2013年 HsiuYi. All rights reserved.
+//
+
+#import "messageViewController.h"
+
+@interface messageViewController ()
+
+@end
+
+@implementation messageViewController
+
+/*
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+ */
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
